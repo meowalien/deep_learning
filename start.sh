0ab5c2447b4f63f00b4c7a849b51b1b6bcbfbf98
say ken <a.meowalien@gmail.com>
2024-11-28 00:29:35 +0800

    edit note

--- start.sh
+++ start.sh
@@ -12,3 +12,9 @@ jupyter lab password
 jupyter lab --ip 0.0.0.0 --no-browser
 
 ngrok http --url=pelican-enormous-tightly.ngrok-free.app 8888
+
+pmset -g
+
+sudo pmset -a disablesleep 1
+
+sudo pmset -a disablesleep 0
