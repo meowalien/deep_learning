d89e8c763e07d908cbf7f4a3000defb542b44fd9
say ken <a.meowalien@gmail.com>
2024-11-27 23:58:27 +0800

    add note

--- start.sh
+++ start.sh
@@ -4,4 +4,11 @@ python -m venv myenv
 source ./myenv/bin/activate
 pip install jupyterlab
 
-jupyter lab
+
+jupyter lab --generate-config
+
+jupyter lab password
+
+jupyter lab --ip 0.0.0.0 --no-browser
+
+ngrok http --url=pelican-enormous-tightly.ngrok-free.app 8888
